6b5e6b5201458e85b5de6aa60e37254112719a46
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-01-15 22:09:56 -0800

    Create windows_build_qt.ps1

---
+++ windows_build_qt.ps1
@@ -0,0 +1,66 @@
+
+# Sanity checks
+if(!$destDir){
+  throw "'destDir' variable not set."
+}
+if(!($buildType -match "^(Debug|Release)$")){
+  throw "'buildType' variable incorrectly set to [$buildType]. Hint: 'Release' or 'Debug' value is expected."
+}
+
+$qtBuildScriptVersion = '53af124b46e6aade989b1bbecbcb8885a11d705c'
+
+if (![System.IO.Directory]::Exists($destDir)) {[System.IO.Directory]::CreateDirectory($destDir)}
+
+cinst jom
+cinst StrawberryPerl
+
+function Download-File {
+param (
+  [string]$url,
+  [string]$file
+  )
+  if (![System.IO.File]::Exists($file)) {
+    Write-Host "Downloading $url to $file"
+    $downloader = new-object System.Net.WebClient
+    $downloader.DownloadFile($url, $file)
+  }
+}
+
+# download 7zip
+Write-Host "Download 7Zip commandline tool"
+$7zaExe = Join-Path $destDir '7za.exe'
+Download-File 'https://github.com/chocolatey/chocolatey/blob/master/src/tools/7za.exe?raw=true' "$7zaExe"
+
+# download CMake
+Write-Host "Download CMake commandline tool"
+$cmakeBaseName = 'cmake-2.8.12.1-win32-x86'
+$cmakeArchiveName = $cmakeBaseName + '.zip'
+$cmakeInstallDir = Join-Path $destDir $cmakeBaseName
+$cmakeArchiveUrl = 'http://www.cmake.org/files/v2.8/' + $cmakeArchiveName
+$cmakeArchiveFile = Join-Path $destDir $cmakeArchiveName
+Download-File $cmakeArchiveUrl $cmakeArchiveFile
+
+# extract CMake package
+if (![System.IO.Directory]::Exists($cmakeInstallDir)) {
+  Write-Host "Extracting $cmakeArchiveFile to $destDir..."
+  Start-Process "$7zaExe" -ArgumentList "x -o`"$destDir`" -y `"$cmakeArchiveFile`"" -Wait
+}
+$cmake = Join-Path $cmakeInstallDir 'bin\cmake.exe'
+
+# download cross-platform build script
+$qtBuildScriptName = 'build_qt_with_openssl.cmake'
+$qtBuildScriptFile = Join-Path $destDir $qtBuildScriptName
+Download-File ('https://raw2.github.com/jcfr/qt-easy-build/' + $qtBuildScriptVersion + '/cmake/' + $qtBuildScriptName) $qtBuildScriptFile
+
+pushd $destDir
+
+Start-Process "$cmake" -ArgumentList `
+  "-DCMAKE_BUILD_TYPE:STRING=$buildType",`
+  "-DDEST_DIR:PATH=$destDir",`
+  "-P", "$qtBuildScriptFile"`
+  -NoNewWindow -PassThru -Wait
+
+popd
+
+
+
