jcfr/qt-easy-build

commit 1ce7801457540bca23dbcac9082e0826568b6f4b

Refactor powershell script introducing 'Always-Download-File'
This will allow to potentially download additional cmake scripts
avoiding to duplicate the code removing prevousily downloaded file.

windows_build_qt.ps1
@@ -18,15 +18,26 @@ if (![System.IO.Directory]::Exists($destDir)) {[System.IO.Directory]::CreateDire
 
 cinst jom
 
+function Always-Download-File {
+param (
+  [string]$url,
+  [string]$file
+  )
+  If (Test-Path $file) {
+    Remove-Item $file
+  }
+  Write-Host "Download $url"
+  $downloader = new-object System.Net.WebClient
+  $downloader.DownloadFile($url, $file)
+}
+
 function Download-File {
 param (
   [string]$url,
   [string]$file
   )
   if (![System.IO.File]::Exists($file)) {
-    Write-Host "Downloading $url to $file"
-    $downloader = new-object System.Net.WebClient
-    $downloader.DownloadFile($url, $file)
+    Always-Download-File '$url' '$file'
   }
 }
 
@@ -54,13 +65,8 @@ $cmake = Join-Path $cmakeInstallDir 'bin\cmake.exe'
 # download cross-platform build script
 $qtBuildScriptName = 'build_qt_with_openssl.cmake'
 $qtBuildScriptFile = Join-Path $destDir $qtBuildScriptName
-If (Test-Path $qtBuildScriptFile)
-  {
-  Remove-Item $qtBuildScriptFile
-  }
 $url = ('https://raw.githubusercontent.com/jcfr/qt-easy-build/' + $qtBuildScriptVersion + '/cmake/' + $qtBuildScriptName)
-Write-Host "Download $url"
-Download-File $url $qtBuildScriptFile
+Always-Download-File $url $qtBuildScriptFile
 
 pushd $destDir
 

commit 6b402369be49e8bb464e34232516f4bb95603292

windows_build_qt.ps1: Properly pass parameter to Download-File
This commit fixes a regression introduced in 1ce7801 (Refactor powershell
script introducing 'Always-Download-File')

windows_build_qt.ps1
@@ -37,7 +37,7 @@ param (
   [string]$file
   )
   if (![System.IO.File]::Exists($file)) {
-    Always-Download-File '$url' '$file'
+    Always-Download-File $url $file
   }
 }
 