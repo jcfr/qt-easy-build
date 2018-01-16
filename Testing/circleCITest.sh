81fdacb36d371fd0d24d7bf243e68af8ba5351c0
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-01-16 11:16:42 -0500

    test: Update CircleCI to use circleCITest.sh script

---
+++ Testing/circleCITest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+root_dir=${script_dir}/..
+
+mkdir -p ${root_dir}/qt-easy-build-build
+
+docker run \
+    -v ${root_dir}:/usr/src/qt-easy-build \
+    -v ${root_dir}/qt-easy-build-build:/usr/src/qt-easy-build-build \
+  fbudin69500/qt-easy-build-test \
+    /usr/src/qt-easy-build/Testing/Docker/test.sh \
+      -j 4 -c -q "5.9.1" \
+      -t "module-qtbase module-qtbase-install_subtargets"
