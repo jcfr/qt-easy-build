bd854a011323cd790e32a6446e978d8a9e1e0824
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-01-31 23:38:29 -0500

    travis: Update list of targets to match the one used on CircleCI

--- Testing/travisCITest.sh
+++ Testing/travisCITest.sh
@@ -17,13 +17,59 @@ then
   -s macosx10.11 \
   -a x86_64 \
   -d 10.9 \
-  -t "sub-tools-bootstrap-all-ordered sub-moc-all-ordered sub-rcc-all-ordered sub-uic-all-ordered sub-corelib-all-ordered sub-gui-all-ordered install_subtargets install_qmake"
+  -t "sub-tools-bootstrap-make_default-ordered \
+sub-moc-make_default-ordered \
+sub-rcc-make_default-ordered \
+sub-uic-make_default-ordered \
+sub-corelib-make_default-ordered \
+sub-xml-make_default-ordered \
+sub-network-make_default-ordered \
+sub-sql-make_default-ordered \
+sub-testlib-make_default-ordered \
+sub-gui-make_default-ordered \
+sub-opengl-make_default-ordered \
+sub-xmlpatterns-make_default-ordered \
+sub-multimedia-make_default-ordered \
+sub-svg-make_default-ordered \
+sub-script-make_default-ordered \
+sub-declarative-make_default-ordered \
+sub-webkit-make_default-ordered \
+sub-scripttools-make_default-ordered \
+sub-plugins-make_default-ordered \
+sub-imports-make_default-ordered \
+sub-tools-make_default-ordered \
+sub-translations-make_default-ordered \
+install_subtargets \
+install_qmake"
 else
   $script_dir/../Build-qt.sh \
   -c \
   -j 4 \
   -y \
-  -t "sub-tools-bootstrap-all-ordered sub-moc-all-ordered sub-rcc-all-ordered sub-uic-all-ordered sub-corelib-all-ordered sub-gui-all-ordered install_subtargets install_qmake"
+  -t "sub-tools-bootstrap-make_default-ordered \
+sub-moc-make_default-ordered \
+sub-rcc-make_default-ordered \
+sub-uic-make_default-ordered \
+sub-corelib-make_default-ordered \
+sub-xml-make_default-ordered \
+sub-network-make_default-ordered \
+sub-sql-make_default-ordered \
+sub-testlib-make_default-ordered \
+sub-gui-make_default-ordered \
+sub-opengl-make_default-ordered \
+sub-xmlpatterns-make_default-ordered \
+sub-multimedia-make_default-ordered \
+sub-svg-make_default-ordered \
+sub-script-make_default-ordered \
+sub-declarative-make_default-ordered \
+sub-webkit-make_default-ordered \
+sub-scripttools-make_default-ordered \
+sub-plugins-make_default-ordered \
+sub-imports-make_default-ordered \
+sub-tools-make_default-ordered \
+sub-translations-make_default-ordered \
+install_subtargets \
+install_qmake"
 fi
 
 die() {
