d8f14535a07b41e0e408f005f7baaf517dd01446
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-02-01 19:09:18 -0500

    travis: De-duplicate list of targets used in test

--- Testing/travisCITest.sh
+++ Testing/travisCITest.sh
@@ -7,6 +7,31 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd $script_dir
 
+targets="sub-tools-bootstrap-make_default-ordered \
+sub-moc-make_default-ordered \
+sub-rcc-make_default-ordered \
+sub-uic-make_default-ordered \
+sub-corelib-make_default-ordered \
+sub-xml-make_default-ordered \
+sub-network-make_default-ordered \
+sub-sql-make_default-ordered \
+sub-testlib-make_default-ordered \
+sub-gui-make_default-ordered \
+sub-opengl-make_default-ordered \
+sub-xmlpatterns-make_default-ordered \
+sub-multimedia-make_default-ordered \
+sub-svg-make_default-ordered \
+sub-script-make_default-ordered \
+sub-declarative-make_default-ordered \
+sub-webkit-make_default-ordered \
+sub-scripttools-make_default-ordered \
+sub-plugins-make_default-ordered \
+sub-imports-make_default-ordered \
+sub-tools-make_default-ordered \
+sub-translations-make_default-ordered \
+install_subtargets \
+install_qmake"
+
 if [ "$(uname)" == "Darwin" ]
 then
   # MacOS
@@ -15,59 +40,13 @@ then
   -j 4 \
   -y \
   -d 10.9 \
-  -t "sub-tools-bootstrap-make_default-ordered \
-sub-moc-make_default-ordered \
-sub-rcc-make_default-ordered \
-sub-uic-make_default-ordered \
-sub-corelib-make_default-ordered \
-sub-xml-make_default-ordered \
-sub-network-make_default-ordered \
-sub-sql-make_default-ordered \
-sub-testlib-make_default-ordered \
-sub-gui-make_default-ordered \
-sub-opengl-make_default-ordered \
-sub-xmlpatterns-make_default-ordered \
-sub-multimedia-make_default-ordered \
-sub-svg-make_default-ordered \
-sub-script-make_default-ordered \
-sub-declarative-make_default-ordered \
-sub-webkit-make_default-ordered \
-sub-scripttools-make_default-ordered \
-sub-plugins-make_default-ordered \
-sub-imports-make_default-ordered \
-sub-tools-make_default-ordered \
-sub-translations-make_default-ordered \
-install_subtargets \
-install_qmake"
+  -t "${targets}"
 else
   $script_dir/../Build-qt.sh \
   -c \
   -j 4 \
   -y \
-  -t "sub-tools-bootstrap-make_default-ordered \
-sub-moc-make_default-ordered \
-sub-rcc-make_default-ordered \
-sub-uic-make_default-ordered \
-sub-corelib-make_default-ordered \
-sub-xml-make_default-ordered \
-sub-network-make_default-ordered \
-sub-sql-make_default-ordered \
-sub-testlib-make_default-ordered \
-sub-gui-make_default-ordered \
-sub-opengl-make_default-ordered \
-sub-xmlpatterns-make_default-ordered \
-sub-multimedia-make_default-ordered \
-sub-svg-make_default-ordered \
-sub-script-make_default-ordered \
-sub-declarative-make_default-ordered \
-sub-webkit-make_default-ordered \
-sub-scripttools-make_default-ordered \
-sub-plugins-make_default-ordered \
-sub-imports-make_default-ordered \
-sub-tools-make_default-ordered \
-sub-translations-make_default-ordered \
-install_subtargets \
-install_qmake"
+    -t "${targets}"
 fi
 
 die() {
