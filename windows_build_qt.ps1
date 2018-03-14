efb0181193d21b2d7a44e62405e5dae5808b18a3
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-03-14 03:42:28 -0400

    windows_build_qt.ps1: Set securityProtocol before downloading. See #35

--- windows_build_qt.ps1
+++ windows_build_qt.ps1
@@ -24,9 +24,24 @@ param (
   If (Test-Path $file) {
     Remove-Item $file
   }
+
+  $securityProtocolSettingsOriginal = [System.Net.ServicePointManager]::SecurityProtocol
+
+  try {
+    # Set TLS 1.2 (3072), then TLS 1.1 (768), then TLS 1.0 (192), finally SSL 3.0 (48)
+    # Use integers because the enumeration values for TLS 1.2 and TLS 1.1 won't
+    # exist in .NET 4.0, even though they are addressable if .NET 4.5+ is
+    # installed (.NET 4.5 is an in-place upgrade).
+    [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48
+  } catch {
+    Write-Warning 'Unable to set PowerShell to use TLS 1.2 and TLS 1.1 due to old .NET Framework installed. If you see underlying connection closed or trust errors, you may need to do one or more of the following: (1) upgrade to .NET Framework 4.5 and PowerShell v3, (2) specify internal Chocolatey package location (set $env:chocolateyDownloadUrl prior to install or host the package internally), (3) use the Download + PowerShell method of install. See https://chocolatey.org/install for all install options.'
+  }
+
   Write-Host "Download $url"
   $downloader = new-object System.Net.WebClient
   $downloader.DownloadFile($url, $file)
+
+  [System.Net.ServicePointManager]::SecurityProtocol = $securityProtocolSettingsOriginal
 }
 
 function Download-File {
