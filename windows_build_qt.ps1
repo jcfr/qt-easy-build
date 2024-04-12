54ec7ca5cf2efff4109e753fab87cf87593255cf
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2024-04-12 17:30:49 -0400

    windows_build_qt.ps1: Update cmake from 2.8.12 to 3.22.6

--- windows_build_qt.ps1
+++ windows_build_qt.ps1
@@ -76,10 +76,10 @@ $jom = Join-Path $jomInstallDir 'jom.exe'
 
 # download CMake
 Write-Host "Download CMake commandline tool"
-$cmakeBaseName = 'cmake-2.8.12.1-win32-x86'
+$cmakeBaseName = 'cmake-3.22.6-windows-x86_64'
 $cmakeArchiveName = $cmakeBaseName + '.zip'
 $cmakeInstallDir = Join-Path $destDir $cmakeBaseName
-$cmakeArchiveUrl = 'http://www.cmake.org/files/v2.8/' + $cmakeArchiveName
+$cmakeArchiveUrl = 'https://www.cmake.org/files/v3.22/' + $cmakeArchiveName
 $cmakeArchiveFile = Join-Path $destDir $cmakeArchiveName
 Download-File $cmakeArchiveUrl $cmakeArchiveFile
 
