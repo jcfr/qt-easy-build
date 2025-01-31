94847c954c7c24f074df493c1e11f04ca6212827
James Butler <james.butler@revvity.com>
2025-01-31 08:26:01 -0500

    Build-qt.sh: Fix QWebEngine build applying patch for newer ninja usage
    This issue started when using ninja version 1.12 as reports indicated that this newer version builds in a different order and there was a missing dependency.
    
    This backports the same type patch applied at:
    - https://github.com/microsoft/vcpkg/commit/0b4805abfa6ad4c5a4f7fe7d067fdab8449fb915

--- Build-qt.sh
+++ Build-qt.sh
@@ -395,6 +395,9 @@ then
     echo "Cloning qtlocation so that patches can be applied with git"
     rm -r qtlocation
     git clone --recurse-submodules https://github.com/qt/qtlocation.git -b v5.15.16-lts-lgpl qtlocation
+    echo "Cloning qtwebengine so that patches can be applied with git"
+    rm -r qtwebengine
+    git clone --recurse-submodules https://github.com/qt/qtwebengine.git -b v5.15.16-lts-lgpl qtwebengine
     echo "Found $patch_count patches"
     git apply --ignore-whitespace $script_dir/patches/*.patch
   fi
